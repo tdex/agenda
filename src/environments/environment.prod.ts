d21bc3b2471c7278052afd2633bfc6ef85ee9a38
Tarcisio Deschamps Silva <tardchamps94@gmail.com>
2019-09-08 10:27:30 -0300

    fix build config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebase = {
+  apiKey: 'AIzaSyBz-Xr_FSSzfXNAx2P5ezyOdK822QCvUmo',
+  authDomain: 'agenda-tdex.firebaseapp.com',
+  databaseURL: 'https://agenda-tdex.firebaseio.com',
+  projectId: 'agenda-tdex',
+  storageBucket: 'agenda-tdex.appspot.com',
+  messagingSenderId: '238749699501',
+  appId: '1:238749699501:web:9d9cd32dcd35cd0ef06524'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false
 };
@@ -15,11 +11,3 @@ export const firebase = {
   messagingSenderId: '238749699501',
   appId: '1:238749699501:web:9d9cd32dcd35cd0ef06524'
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
