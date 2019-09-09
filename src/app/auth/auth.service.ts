bb48b14aa3ac3d253e9feaa3cb1183bc607b09b0
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2019-09-09 13:38:00 -0300

    update login social

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
   user: User;
 
   constructor(public afAuth: AngularFireAuth, public router: Router) {
+    this.afAuth.auth.languageCode = 'pt';
     this.afAuth.authState.subscribe(user => {
       if (user) {
         this.user = user;
@@ -28,6 +29,12 @@ export class AuthService {
     } catch (error) {
       alert('Error!' + error.message);
     }
+  }
+
+  async loginFacebook() {
+    await this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider()).then(response => {
+      console.log('LogIn com facebook', response.user.email);
+    }).catch(error => console.error(error));
   }
 
   async logout() {
